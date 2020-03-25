48171771b9e1328c56936363d4d2bc6cba5f539e
Blend Ibraj <blendi.ibraj96@gmail.com>
2020-03-25 19:51:22 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,12 +5,12 @@ def key_for_min_value(name_hash)
   if name_hash.length == 0
     return nil
   else
-    array = []
+    arr = []
     name_hash.each do |key, value|
-      array << value
+      arr << value
     end
-    min_num = array[0]
-    array.each do |value|
+    min_num = arr[0]
+    arr.each do |value|
       min_num = value if value < min_num
     end
     name_hash.each do |key, value|
